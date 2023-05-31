b7a43bb213a958a226f3c54d435f9505e46b3451
Sutak0 <sunsamu05@gmail.com>
2023-05-31 09:33:25 +0200

    felido

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Adatok
+    {
+        public string Ev;
+        public string Nev;
+        public string Vegyjel;
+        public int Rendszam;
+        public string Felfedezo;
+
+        public Adatok() {
+            
+            string[] adatok = x.Split(";");
+            this.Ev = adatok[0];
+            this.Nev = adatok[1];
+            this.Vegyjel = adatok[2];
+            this.Rendszam = Convert.ToInt32(adatok[3]);
+            this.Felfedezo = (adatok[4]);
+
+        
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -10,6 +11,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            List <Adatok> ujlista = new List <Adatok> ();
+
+            using (StreamReader sr = new StreamReader("felfedezesek.csv"))
+            {
+
+                sr.ReadLine();
+                do
+                {
+                    string sor = sr.ReadLine();
+                    Adatok sor1 = new Adatok();
+                    ujlista.Add(sor1);
+
+                } while (!sr.EndOfStream);
+
+                Console.WriteLine($"3-as feladat: \t Kemia elemek felfedezesi adatai:  {ujlista.Count} ");
+
+                int Okor = ujlista.Where(x=>x.Ev == "Okor").Count();
+                Console.WriteLine($"4-es feladat: \t Az ókorbn felfedezett kémiai elemek száma: {Okor}");
+
+                //5 
+                string betujel = sr.ReadLine();
+                while (true)
+                {
+
+                }
+
+                //6
+                if (true)
+                {
+                    int vegyjel = ujlista.Where(x => x.Vegyjel == "SG" && x.Vegyjel == "sg").Count();
+                    Console.WriteLine(${ });
+                }
+                 
+                   
+                
+                
+               
+            }
         }
     }
 }
